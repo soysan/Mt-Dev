96e2a6147e5d55c4cc8d18ca4c801af40f8e6d5c
Masayuki <soy2.san@gmail.com>
2021-08-29 02:26:48 +0900

    Categories and ThirdParties

--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -22,3 +22,21 @@ type Stacks =
 export interface ProjectKey {
   [key: string]: ProjectProps;
 }
+
+export interface CategoryProps {
+  title: string;
+  img: string;
+}
+
+export interface MainCardProps {
+  [key: string]: CategoryProps;
+}
+
+export interface ThirdPartyProps {
+  title: string;
+  url: string;
+}
+
+export interface ThirdProps {
+  [key: string]: ThirdPartyProps;
+}
