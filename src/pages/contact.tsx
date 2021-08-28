02574e75b75dde4f5c72e88a5f7c14e7e7872131
Masayuki <soy2.san@gmail.com>
2021-08-29 02:27:22 +0900

    extra pages

---
+++ src/pages/contact.tsx
@@ -0,0 +1,17 @@
+import React, { VFC } from 'react';
+import Layout from '@/components/Layout';
+import { Typography } from '@material-ui/core';
+
+const Contact: VFC = () => {
+  return (
+    <>
+      <Layout>
+        <Typography variant='h2' component='h1'>
+          [Coming soon]
+        </Typography>
+      </Layout>
+    </>
+  );
+};
+
+export default Contact;
---
+++ src/pages/profile.tsx
@@ -0,0 +1,17 @@
+import Layout from '@/components/Layout';
+import { Typography } from '@material-ui/core';
+import React, { VFC } from 'react';
+
+const Profile: VFC = () => {
+  return (
+    <>
+      <Layout>
+        <Typography variant='h2' component='h1'>
+          [Coming soon]
+        </Typography>
+      </Layout>
+    </>
+  );
+};
+
+export default Profile;
